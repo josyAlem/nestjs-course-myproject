770510c729a60f9015c501ec94a424c11fcd6fda
josyAlem <yoseph.click@gmail.com>
2021-09-02 17:39:23 +0300

    added cors

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,18 @@ import { AppPaths } from './appPaths';
 import { envVariables } from "src/shared/config/env.enums";
 
 const port: string = envVariables.PORT;
+const client_addr: string = envVariables.CLIENT_ADDRESS;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+ 
   app.useStaticAssets(AppPaths.publicPath, {
     prefix: '/public'
   });
-  const configService = app.get(ConfigService);
+
+  app.enableCors({origin:[configService.get(client_addr)]});
+
   await app.listen(configService.get(port));
 }
 bootstrap();
