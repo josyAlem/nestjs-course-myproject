af1c6a23338156f2250ee7872599831d401f922e
josyAlem <yoseph.click@gmail.com>
2021-09-06 19:13:32 +0300

    --

--- src/appPaths.ts
+++ src/appPaths.ts
@@ -3,6 +3,6 @@ import { join } from "path";
 export const AppPaths={
   publicPath:join("src","..","public"),
   imagesPath:"images",
-  staticFrontendDir:join("src","..","angular"),
-  staticFrontendFile:join("src","..","angular","index.html")
+  staticFrontendDir:join("src","..","front-end"),
+  staticFrontendFile:join("src","..","front-end","index.html")
 }
